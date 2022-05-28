10d39396ff94691701bc0e11a898db1069b57b51
hcslearning <learning@hardcybersoft.cl>
2022-05-28 01:00:05 -0400

    file upload example

---
+++ codigo/prueba-fileupload/src/main/java/cl/aiep/java/file/PruebaFileuploadApplication.java
@@ -0,0 +1,13 @@
+package cl.aiep.java.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaFileuploadApplication.class, args);
+	}
+
+}
---
+++ codigo/prueba-fileupload/src/main/java/cl/aiep/java/file/entity/Archivo.java
@@ -0,0 +1,29 @@
+package cl.aiep.java.file.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Archivo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String filename;
+	private String tipo;
+	@Lob
+	private byte[] datos;
+	
+}
